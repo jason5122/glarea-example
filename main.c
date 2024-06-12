d50b2239370cffa5f25ea5d7e3eab16435888d52
Jason Han <jjhan@alum.vassar.edu>
2024-06-12 13:31:55 -0400

    Successfully share textures and render GLArea!

--- main.c
+++ main.c
@@ -10,17 +10,30 @@ static gboolean realize(GtkGLArea* self, gpointer user_data) {
     fprintf(stderr, "tex_id: %d\n", tex_id);
 }
 
+static gboolean render(GtkGLArea* self, GdkGLContext* context, gpointer user_data) {
+    gtk_gl_area_make_current(self);
+
+    glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw commands are flushed after returning.
+    return true;
+}
+
 static GtkWidget* create_window(GtkApplication* app) {
     GtkWidget* window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
     GtkWidget* gtk_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_window_set_child(GTK_WINDOW(window), gtk_box);
+
     GtkWidget* gl_area = gtk_gl_area_new();
+    gtk_widget_set_hexpand(gl_area, TRUE);
+    gtk_widget_set_vexpand(gl_area, TRUE);
     gtk_box_append(GTK_BOX(gtk_box), gl_area);
     g_signal_connect(gl_area, "realize", G_CALLBACK(realize), NULL);
-
-    gtk_window_set_child(GTK_WINDOW(window), gtk_box);
+    g_signal_connect(gl_area, "render", G_CALLBACK(render), NULL);
 
     return window;
 }
