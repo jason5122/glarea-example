3623e39ce0c230ca8ebafc8a1724cdb1d410cf1b
Jason Han <jjhan@alum.vassar.edu>
2024-06-12 12:25:50 -0400

    Create minimum example of texture sharing

--- Makefile
+++ Makefile
@@ -1,42 +1,21 @@
-PREFIX = /usr/local
-
-V ?= 0
-V_GEN = $(V__gen_V_$(V))
-V__gen_V_0 = @echo " GEN    " $@;
-V__gen_V_1 =
-
-V_CC = $(V__cc_V_$(V))
-V__cc_V_0 = @echo " CC     " $@;
-V__cc_V_1 =
-
-V_LINK = $(V__link_V_$(V))
-V__link_V_0 = @echo " LINK   " $@;
-V__link_V_1 =
-
 CC = gcc -std=c99
 PKGCONFIG = $(shell which pkg-config)
 CFLAGS = $(shell $(PKGCONFIG) --cflags gio-2.0 gtk+-3.0 epoxy)
 LIBS = $(shell $(PKGCONFIG) --libs gio-2.0 gtk+-3.0 epoxy) -lm 
-GLIB_COMPILE_RESOURCES = $(shell $(PKGCONFIG) --variable=glib_compile_resources gio-2.0)
-GLIB_COMPILE_SCHEMAS = $(shell $(PKGCONFIG) --variable=glib_compile_schemas gio-2.0)
 
-SRC = glarea-app.c glarea-app-window.c glarea-error.c main.c
-GEN = glarea-resources.c
+SRC = main.c
 BIN = glarea
 
-ALL = $(GEN) $(SRC)
+ALL = $(SRC)
 OBJS = $(ALL:.c=.o)
 
 all: $(BIN)
 
-glarea-resources.c: glarea.gresource.xml $(shell $(GLIB_COMPILE_RESOURCES) --sourcedir=. --generate-dependencies glarea.gresource.xml)
-	$(V_GEN)$(GLIB_COMPILE_RESOURCES) glarea.gresource.xml --target=$@ --sourcedir=. --generate-source
-
 %.o: %.c
-	$(V_CC)$(CC) $(CFLAGS) -c -o $(@F) $<
+	$(CC) $(CFLAGS) -c -o $(@F) $<
 
 $(BIN): $(OBJS)
-	$(V_LINK)$(CC) -o $(@F) $(OBJS) $(LIBS)
+	$(CC) -o $(@F) $(OBJS) $(LIBS)
 
 clean:
-	@rm -f $(GEN) $(OBJS) $(BIN)
+	@rm -f $(OBJS) $(BIN)
--- main.c
+++ main.c
@@ -1,7 +1,41 @@
+#include <epoxy/gl.h>
 #include <gtk/gtk.h>
+#include <stdio.h>
 
-#include "glarea-app.h"
+static gboolean realize(GtkGLArea* self, gpointer user_data) {
+    gtk_gl_area_make_current(self);
 
-int main(int argc, char* argv[]) {
-    return g_application_run(G_APPLICATION(glarea_app_new()), argc, argv);
+    GLuint tex_id;
+    glGenTextures(1, &tex_id);
+    fprintf(stderr, "tex_id: %d\n", tex_id);
+}
+
+static GtkWidget* create_window(GtkApplication* app) {
+    GtkWidget* window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    GtkWidget* gl_area = gtk_gl_area_new();
+    gtk_container_add(GTK_CONTAINER(window), gl_area);
+    g_signal_connect(gl_area, "realize", G_CALLBACK(realize), NULL);
+    return window;
+}
+
+static void activate(GtkApplication* app, gpointer user_data) {
+    GtkWidget* window1 = create_window(app);
+    GtkWidget* window2 = create_window(app);
+
+    gtk_widget_show_all(window1);
+    gtk_widget_show_all(window2);
+}
+
+int main(int argc, char** argv) {
+    GtkApplication* app;
+    int status;
+
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
+    return status;
 }
